9f04629a66c3c0098c1684b79073226e4f50aaf5
pranaypj1o3 <pranayjakkannawar@gmail.com>
2024-08-10 11:15:14 +0000

    M1

---
+++ src/main/java/jspiders/App.java
@@ -0,0 +1,13 @@
+package jspiders;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
